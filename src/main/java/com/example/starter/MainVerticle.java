e49eecd8e3bb2fceeb4be0352e4af7eb684a619f
JungHoe <51188350+JungHoe@users.noreply.github.com>
2019-07-18 10:26:40 +0900

    REST 업데이트
    일단완료

--- src/main/java/com/example/starter/MainVerticle.java
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -29,16 +29,17 @@ public class MainVerticle extends AbstractVerticle {
 	@Override
 	public void start(Future<Void> startFuture) throws Exception {
 		System.out.println("Vert.x is run...");
-		
-		Router router = Router.router(vertx);	//vert.x 라우터연결
-		HttpServer server = vertx.createHttpServer();	//vert.x 서버 생성
-		JsonObject config = new JsonObject()	//DataSource 객체 생성
-				.put("url","jdbc:mysql://14.49.38.55:3306/kbsystodo?useUnicode=true&characterEncoding=utf8&serverTimezone=UTC&useSSL=false")
+
+		Router router = Router.router(vertx); // vert.x 라우터연결
+		HttpServer server = vertx.createHttpServer(); // vert.x 서버 생성
+		JsonObject config = new JsonObject() // DataSource 객체 생성
+				.put("url",
+						"jdbc:mysql://14.49.38.55:3306/kbsystodo?useUnicode=true&characterEncoding=utf8&serverTimezone=UTC&useSSL=false")
 				.put("driver_class", "com.mysql.jdbc.Driver").put("user", "kbsystodo").put("password", "kbsystodo");
-		
-		SQLClient client = JDBCClient.createShared(vertx, config); 	//SQL Client 생성
-		
-		Set<String> allowedHeaders = new HashSet<>();			//CORS 
+
+		SQLClient client = JDBCClient.createShared(vertx, config); // SQL Client 생성
+
+		Set<String> allowedHeaders = new HashSet<>(); // CORS
 		allowedHeaders.add("x-requested-with");
 		allowedHeaders.add("Access-Control-Allow-Origin");
 		allowedHeaders.add("origin");
@@ -48,67 +49,136 @@ public class MainVerticle extends AbstractVerticle {
 		Set<HttpMethod> allowedMethods = new HashSet<>();
 		allowedMethods.add(HttpMethod.GET);
 		allowedMethods.add(HttpMethod.POST);
+		allowedMethods.add(HttpMethod.DELETE);
 		allowedMethods.add(HttpMethod.PATCH);
 		router.route("/insert").handler(BodyHandler.create());
-		router.route().handler(CorsHandler.create("*")	//Cors handler
-		.allowedHeaders(allowedHeaders).allowedMethods(allowedMethods));
+		router.route().handler(CorsHandler.create("*") // Cors handler
+				.allowedHeaders(allowedHeaders).allowedMethods(allowedMethods));
 
-		//Mapping주소 "/" get method REST API
+		// Mapping주소 "/" get method REST API
 		router.get("/").handler(routingContext -> {
-			Map<String, Object> result = new HashMap<String, Object>();	//결과 담는 객체
+			Map<String, Object> result = new HashMap<String, Object>(); // 결과 담는 객체
 			HttpServerResponse response = routingContext.response();
-			
-			response.putHeader("content-type", "application/json");	//json 타입으로 
-					
-			client.getConnection(res -> {	//DB와연결
-				if (res.succeeded()) {	//접속성공일시
-					
+
+			response.putHeader("content-type", "application/json"); // json 타입으로
+
+			client.getConnection(res -> { // DB와연결
+				if (res.succeeded()) { // 접속성공일시
+
 					SQLConnection connection = res.result();
-					connection.query("select id from todoList1 ", res2 -> {	//TotalCnt 만드는 쿼리
+					connection.query("select id from todoList1 ", res2 -> { // TotalCnt 만드는 쿼리
 						ResultSet rs = res2.result();
 						result.put("totalCnt", rs.getNumRows());
-						
+
 					});
-					
-					connection.query("select id,text,checked,color,"
-							+ "moment from todoList1 where useYn='Y' ", res3 -> {	//전체테이블 가져오는 쿼리
-						ResultSet rs = res3.result();
-						result.put("todoList", rs.getRows());
-						response.end(Json.encodePrettily(result));		//result값을 리턴
-						
-					});
+
+					connection.query("select id,text,checked,color," + "moment from todoList1 where useYn='Y' ",
+							res3 -> { // 전체테이블 가져오는 쿼리
+								ResultSet rs = res3.result();
+								result.put("todoList", rs.getRows());
+								response.end(Json.encodePrettily(result)); // result값을 리턴
+
+							});
 				} else {
 					System.out.println("접속실패");
 					// Failed to get connection - deal with it
 				}
 			});
 		});
-		//Mapping주소 "/insert" Post method REST API
-		router.post("/insert").handler(routingContext ->{
-			System.out.println("insert 들어옴");
+		// Mapping주소 "/insert" Post method REST API
+		router.post("/insert").handler(routingContext -> {
+//			System.out.println("insert 들어옴");
 			HttpServerRequest request = routingContext.request();
-			
-			JsonArray params=new JsonArray()
-					.add(request.getParam("id")).add(request.getParam("text")).add(request.getParam("color"));
-		
-	
-			client.getConnection(res -> {	//DB와연결
-				if (res.succeeded()) {	//접속성공일시
+
+			JsonArray params = new JsonArray().add(request.getParam("id")).add(request.getParam("text"))
+					.add(request.getParam("color"));
+
+			client.getConnection(res -> { // DB와연결
+				if (res.succeeded()) { // 접속성공일시
 					SQLConnection connection = res.result();
-					connection.updateWithParams("insert into todoList1 (id,text,color) values (?,?,?)", params, e->{
-						UpdateResult updateResult = e.result();
-						System.out.println("No. of rows inserted: " + updateResult.getUpdated());
+					connection.updateWithParams("insert into todoList1 (id,text,color) values (?,?,?)", params, e -> {
+						// 필요시 핸들러 작성
+//						UpdateResult updateResult = e.result();
+//						System.out.println("No. of rows inserted: " + updateResult.getUpdated());
 					});
-				}else {
-					
+				} else {
+
 				}
 			});
+
+		});
+
+		// Mapping주소 "/checked" patch method REST API
+		router.patch("/checked").handler(routingContext -> {
+//System.out.println("체크 들어옴");
+			HttpServerRequest request = routingContext.request();
+
+			JsonArray params = new JsonArray() // update parameters 생성
+					.add(request.getParam("checked")).add(request.getParam("id"));
+
+			client.getConnection(res -> { // DB와연결
+				if (res.succeeded()) { // 접속성공일시
+
+					SQLConnection connection = res.result();
+					connection.updateWithParams("update todoList1 set checked = ? where id = ?", params, e -> {
+						// 필요시 핸들러 작성
+//						UpdateResult updateResult = e.result();
+//						System.out.println("No. of rows updated: " + updateResult.getUpdated());
+					});
+				} else {
+
+				}
+			});
+
+		});
+		// Mapping주소 "/delete" delete method REST API
+		router.delete("/delete").handler(routingContext -> {
+//			System.out.println("딜리트 들어옴");
+			HttpServerRequest request = routingContext.request();
+
+			JsonArray params = new JsonArray() // update parameters 생성
+					.add(request.getParam("id"));
 			
-	
+			client.getConnection(res -> { // DB와연결
+				if (res.succeeded()) { // 접속성공일시
+					SQLConnection connection = res.result();
+					connection.updateWithParams("update todoList1 set useYn = 'N' where id = ?", params, e -> {
+						// 필요시 핸들러 작성
+						UpdateResult updateResult = e.result();
+						System.out.println("No. of rows updated: " + updateResult.getUpdated());
+					});
+				} else {
+
+				}
+			});
+
+		});
+		
+		// Mapping주소 "/todoitem" patch method REST API
+		router.patch("/todoitem").handler(routingContext -> {
+			System.out.println("업데이트 들어옴");
+			HttpServerRequest request = routingContext.request();
+
+			JsonArray params = new JsonArray() // update parameters 생성
+			.add(request.getParam("text")).add(request.getParam("color")).add(request.getParam("checked")).add(request.getParam("id"));
+
+			client.getConnection(res -> { // DB와연결
+				if (res.succeeded()) { // 접속성공일시
+					
+					SQLConnection connection = res.result();
+					connection.updateWithParams("update todoList1 set text = ? , color = ? , checked = ? where id = ?", params, e -> {
+						// 필요시 핸들러 작성
+//						UpdateResult updateResult = e.result();
+//						System.out.println("No. of rows updated: " + updateResult.getUpdated());
+					});
+				} else {
+
+				}
+			});
+
 		});
 
 		server.requestHandler(router).listen(8080);
 	}
-	
 
 }
